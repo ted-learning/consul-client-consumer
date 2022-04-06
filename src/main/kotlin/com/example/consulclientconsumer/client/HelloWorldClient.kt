1740fa4045e5d579cc3c1988d03f877188b92209
Zhang Gang <gzhang@thoughtworks.com>
2022-04-06 14:49:03 +0800

    update: remove used config

--- src/main/kotlin/com/example/consulclientconsumer/client/HelloWorldClient.kt
+++ src/main/kotlin/com/example/consulclientconsumer/client/HelloWorldClient.kt
@@ -2,12 +2,10 @@ package com.example.consulclientconsumer.client
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
-@LoadBalancerClient(name = "say-hello")
 @Configuration
 class HelloWorldClient {
     @Value("\${services.provider}")
